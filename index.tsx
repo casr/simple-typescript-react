cc22341c08431530db52784b4af79e0903501a68
Chris Rawnsley <chris@puny.agency>
2017-06-23 08:44:15 +0100

    Remove unused variable

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { BrowserRouter as Router, Route, Link, RouteComponentProps } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 import Greeter from './greeter'
 import store from './store'
